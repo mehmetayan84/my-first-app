f842d13f4be680df73375d843f5a2a195ee9ed96
Mehmet <mehmetayan84@gmail.com>
2017-12-26 23:26:26 +0300

    databinding, string interpolation is added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer:boolean = false;
+  
+  serverAddStatus:string = "No server is added";
+
+  serverName:string = "";
+
+  constructor() { 
+    setTimeout(() => {this.allowNewServer = true}, 2000);
+  }
 
   ngOnInit() {
   }
 
+  setServerAddStatus(){
+    this.serverAddStatus = "New server is added and the name of it : " + this.serverName;
+  }
+
+  onUpdateServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
